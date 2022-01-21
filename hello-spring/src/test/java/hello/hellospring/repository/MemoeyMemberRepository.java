9a161991d9820815ad1d50de4bcfaa9457ef71e4
du_hy <du_hy@DESKTOP-MOA9DQA>
2022-01-21 17:18:00 +0900

    1.프로젝트 환경설정, 2.스프링 웹 개발 기초, 3.회원 관리 예제 2-1.정적 컨텐츠, 2-2.MVC와 템플릿 엔지, 2-3. API 3-1. 회원 도메인과 리포지토리 만들기

--- hello-spring/src/test/java/hello/hellospring/repository/MemoeyMemberRepository.java
+++ hello-spring/src/test/java/hello/hellospring/repository/MemoeyMemberRepository.java
@@ -25,7 +25,7 @@ class MemoeyMemberRepository {
 	}
 	
 	@Test
-	public void findByName() {
+	public void findByName() { 
 		Member member1 = new Member();
 		member1.setName("spring1");
 		repository.save(member1);
