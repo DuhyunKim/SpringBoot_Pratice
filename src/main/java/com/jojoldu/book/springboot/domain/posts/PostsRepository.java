84a074acaa03c0dd295c9d351b0cb51fe2195270
du_hy <du_hyun4772@naver.com>
2022-03-03 18:53:03 +0900

    머스테치를 이용하여 게시판 만들기 1)등록하기(완료) 2)조회하기(진행 중)

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,7 +1,13 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 //JpaRepository<Posts, Long>의 Posts: Entity 클래스, Long: PK타입
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
